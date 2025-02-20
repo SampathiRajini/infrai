75800fdddc9cb4d08eb1f2b2e5a12fef7e3d86eb
SampathiRajini <127491843+SampathiRajini@users.noreply.github.com>
2025-02-20 18:38:21 +0530

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+region = "ap-south-1"
+}
+
+resource "aws_instance" {
+ ami           ="ami-0ddfba243cbee3768"
+ instance_type ="t2.micro"
+ tags={
+   Name="dev-server"
+   }
+   }
