0c0fc12f9ff6cea74d1ad0b9054217d5beda37b0
SampathiRajini <127491843+SampathiRajini@users.noreply.github.com>
2025-02-20 19:21:03 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,9 +3,9 @@ region = "ap-south-1"
 }
 
 resource "aws_instance" "one" {
- ami           ="ami-0ddfba243cbee3768"
- instance_type ="t2.micro"
- tags={
-   Name="dev-server"
+  ami           = "ami-0ddfba243cbee3768"
+  instance_type = "t2.micro"
+  tags = {
+    Name = "dev-server"
    }
-   }
+ }
